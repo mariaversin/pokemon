1cbdb71d20c311b7b42ce282e3c4d708003fd6ad
maria <verdejosinmaria@gmail.com>
2021-12-08 15:52:30 +0100

    streamlit changes

--- src/quienGanara.py
+++ src/quienGanara.py
@@ -14,6 +14,7 @@ def prediccion (numeroPokemon1, numeroPokemon2,Pokedex):
     pokemon1 = busquedaInformacionPokemon(numeroPokemon1, Pokedex)
     pokemon2 = busquedaInformacionPokemon(numeroPokemon2, Pokedex)
     modelo_prediccion = joblib.load('modelo/modelo_pokemon.mod')
+   
     prediccion_Pokemon_1 = modelo_prediccion.predict([[pokemon1[2],pokemon1[3],pokemon1[4],pokemon1[5],pokemon1[6],pokemon1[7],pokemon1[8]]])
     prediccion_Pokemon_2 = modelo_prediccion.predict([[pokemon2[2], pokemon2[3], pokemon2[4], pokemon2[5], pokemon2[6], pokemon2[7], pokemon2[8]]])
     print ("COMBATE QUE ENFRENTA: ("+str(numeroPokemon1)+") "+pokemon1[1]+" a ("+str(numeroPokemon2)+") "+pokemon2[1])
@@ -21,7 +22,7 @@ def prediccion (numeroPokemon1, numeroPokemon2,Pokedex):
     print("   " +pokemon2[1] + ": " + str(prediccion_Pokemon_2[0]))
     print ("")
     if prediccion_Pokemon_1 > prediccion_Pokemon_2:
-        return pokemon1[1].upper()+" ES EL GANADOR"
+        return pokemon1[1].upper()+ " (" + pokemon1[0] + ") " + "ES EL GANADOR"
     else:
-        return pokemon2[1].upper() + " ES EL GANADOR"
+        return pokemon2[1].upper() +   " (" + pokemon2[0] + ") " + " ES EL GANADOR"
 
--- src/web.py
+++ src/web.py
@@ -1,11 +1,21 @@
 import streamlit as st 
 from quienGanara import prediccion as predic
 import csv
+import pandas as pd 
+import numpy as np 
+import altair as alt
+
 st.title("Enfrenta a dos Pokemon y descubre quién será el ganador")
+num1 = st.number_input("Primer Pokemon", min_value=1, max_value=300)
+st.write(num1)
+num2 = st.number_input("Segundo Pokemon", min_value=1, max_value=300)
+st.write(num2)
+
 
 
 with open("datas/pokedex.csv", newline='') as csvfile:
     pokedex = csv.reader(csvfile) 
     next(pokedex)
-    valores = predic(268,518,pokedex)
+    valores = predic(num1, num2,pokedex)
     st.write(valores)
+
